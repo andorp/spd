58e83c176f5e24aae05e316a66e92dd610c5c45a
andorp <andor.penzes@gmail.com>
2014-07-14 11:38:10 +0200

    Chapter 4.6

---
+++ src/SPD/Book/PartOne/Chapter4_6.hs
@@ -0,0 +1,56 @@
+module Main where
+
+{-
+4.6 Designing With Itemizations
+-}
+
+import SPD.Test
+
+{-
+Sample Problem:
+The state of Tax Land has created a three-stage sales tax to cope with its budget deficit.
+* Inexpensive items, those costing less than $1,000, are not taxed.
+* Luxury items, with a price of more than $10,000, are taxed at the rate of eight percent (8.00%).
+* Everything in between comes with a five percent (5.25%) price tag.
+
+Design a function for a cash register that computes the sales tax for each item.
+That is, design a function that, given the price of an item,
+computes the amount of tax to be charged.
+-}
+
+-- The Tax price falls into on eof the three intervals:
+-- * 0 through 1000
+-- * 1000 through 10000
+-- * 10000 and above
+
+type Price = Double
+
+-- * Constants
+
+normalTax = 5.25 / 100
+luxoryTax = 8.00 / 100
+
+lowPrice    = 1000
+luxoryPrice = 10000
+
+{-
+data Tax = Tax (Price, Price)
+  deriving (Show, Eq)
+-}
+
+salesTax :: Price -> Double
+-- ^ Compute the amount of tax charged for price p
+salesTax x = (tax x) * x where
+  tax p
+    | and [(0 <= p),    (p < lowPrice)] = 0
+    | and [(lowPrice <= p), (p < luxoryPrice)] = normalTax
+    | (p >= luxoryPrice) = luxoryTax
+
+salesTaxTests = do
+  assertEquals "0.0"    0 (salesTax 0) ""
+  assertEquals "Low price - 1"  0 (salesTax (lowPrice - 1)) ""
+  assertEquals "Low price" (normalTax * lowPrice) (salesTax lowPrice) ""
+  assertEquals "Low price + 1" (normalTax * (lowPrice + 1)) (salesTax (lowPrice + 1)) ""
+  assertEquals "Luxory price - 1" (normalTax * (luxoryPrice - 1)) (salesTax (luxoryPrice - 1)) ""
+  assertEquals "Luxory price" (luxoryTax * luxoryPrice) (salesTax luxoryPrice) ""
+  assertEquals "Luxory price + 1" (luxoryTax * (luxoryPrice + 1)) (salesTax (luxoryPrice + 1)) ""
